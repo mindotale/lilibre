e3d883fcc441981a5953ad27883d7e580d2e6f7c
Volodymyr Domin <mindotale@gmail.com>
2023-10-10 20:44:25 +0300

    feat: Add the Persistence layer with a generic Repository

---
+++ src/Api/Lilibre.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Lilibre.Application;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Lilibre.Persistence;
+
+public sealed class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Author> Authors { get; set; } = null!;
+
+    public DbSet<Book> Books { get; set; } = null!;
+
+    public DbSet<Genre> Genres { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
