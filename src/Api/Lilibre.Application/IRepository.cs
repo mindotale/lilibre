c2763a95be2d39fdd98c31feb150efd38355c1a8
Volodymyr Domin <mindotale@gmail.com>
2023-10-10 20:43:44 +0300

    feat: Add IRepository

---
+++ src/Api/Lilibre.Application/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Lilibre.Application;
+
+public interface IRepository<TEntity, TId> where TEntity : IEntity<TId> where TId : IEquatable<TId>
+{
+    Task<TEntity?> GetByIdAsync(TId id);
+
+    Task<IList<TEntity>> GetAllAsync(int offset, int limit);
+
+    Task<TId> AddAsync(TEntity entity);
+
+    Task UpdateAsync(TEntity entity);
+
+    Task<bool> DeleteAsync(TId id);
+}
