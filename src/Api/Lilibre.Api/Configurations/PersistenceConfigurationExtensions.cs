4f9797406090fe34a1e7742e54e1f04c78e3da96
Volodymyr Domin <mindotale@gmail.com>
2023-10-11 14:36:00 +0300

    feat: Add the Persistence layer configuration

---
+++ src/Api/Lilibre.Api/Configurations/PersistenceConfigurationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Lilibre.Application;
+using Lilibre.Persistence;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Lilibre.Api.Configurations;
+
+public static class PersistenceConfigurationExtensions
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(
+            options =>
+            {
+                var connectionString = configuration.GetValue<string>("ConnectionString");
+                options.UseSqlServer(connectionString);
+            });
+        services.AddRepositories();
+
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IRepository<Book, int>, Repository<Book, int>>();
+        services.AddScoped<IRepository<Author, int>, Repository<Author, int>>();
+        services.AddScoped<IRepository<Genre, int>, Repository<Genre, int>>();
+
+        return services;
+    }
+}
--- src/Api/Lilibre.Api/Program.cs
+++ src/Api/Lilibre.Api/Program.cs
@@ -1,8 +1,13 @@
+using Lilibre.Api.Configurations;
+using Lilibre.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
@@ -17,5 +22,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 app.Run();
