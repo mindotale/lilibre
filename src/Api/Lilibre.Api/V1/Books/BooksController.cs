bf2356a8162e8cc87943622a79dd14c1bc942d4d
Volodymyr Domin <mindotale@gmail.com>
2023-10-11 15:46:13 +0300

    feat: Add BooksController

--- src/Api/Lilibre.Api/V1/Books/BooksController.cs
+++ src/Api/Lilibre.Api/V1/Books/BooksController.cs
@@ -1,7 +1,9 @@
-﻿using Lilibre.Api.V1.Authors;
-using Lilibre.Api.V1.Genres;
+﻿using Lilibre.Application;
 
 using Microsoft.AspNetCore.Mvc;
+
+using Author = Lilibre.Api.V1.Authors.Author;
+using Genre = Lilibre.Api.V1.Genres.Genre;
 
 namespace Lilibre.Api.V1.Books;
 
@@ -9,34 +11,138 @@ namespace Lilibre.Api.V1.Books;
 [ApiController]
 public class BooksController : ControllerBase
 {
-    [HttpGet]
-    public Task<ActionResult<IEnumerable<Book>>> GetBooks(int offset = 0, int limit = 10)
+    private readonly IRepository<Application.Book, int> _bookRepository;
+    private readonly IRepository<Application.Author, int> _authorRepository;
+    private readonly IRepository<Application.Genre, int> _genreRepository;
+
+    public BooksController(IRepository<Application.Book, int> bookRepository, IRepository<Application.Author, int> authorRepository, IRepository<Application.Genre, int> genreRepository)
     {
-        throw new NotImplementedException();
+        _bookRepository = bookRepository;
+        _authorRepository = authorRepository;
+        _genreRepository = genreRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Book>>> GetBooks(int offset = 0, int limit = 10)
+    {
+        var books = await _bookRepository.GetAllAsync(offset, limit);
+        var response = books.Select(b => b.ToBook());
+        return Ok(response);
     }
 
     [HttpGet("{id:int}")]
-    public Task<ActionResult<Book>> GetBook(int id)
+    public async Task<ActionResult<Book>> GetBook(int id)
     {
-        throw new NotImplementedException();
+        var book = await _bookRepository.GetByIdAsync(id);
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        var response = book.ToBook();
+        return Ok(response);
     }
 
     [HttpPost]
-    public Task<ActionResult<Book>> CreateBook(CreateBook request)
+    public async Task<ActionResult<Book>> CreateBook(CreateBook request)
     {
-        throw new NotImplementedException();
+        var authors = new List<Application.Author>();
+        foreach (var id in request.Authors)
+        {
+            var author = await _authorRepository.GetByIdAsync(id);
+            if (author is null)
+            {
+                return BadRequest();
+            }
+
+            authors.Add(author);
+        }
+
+        var genres = new List<Application.Genre>();
+        foreach (var id in request.Genres)
+        {
+            var genre = await _genreRepository.GetByIdAsync(id);
+            if (genre is null)
+            {
+                return BadRequest();
+            }
+
+            genres.Add(genre);
+        }
+
+        var book = new Application.Book
+        {
+            Id = 0,
+            Title = request.Title,
+            Authors = authors,
+            Genres = genres,
+            Description = request.Description,
+            Isbn = request.Isbn,
+            Price = request.Price,
+            Pages = request.Pages,
+            Year = request.Year
+        };
+
+        var bookId = await _bookRepository.AddAsync(book);
+        var response = book.ToBook();
+        return CreatedAtAction(nameof(GetBook), new { id = bookId }, response);
     }
 
     [HttpPut("{id:int}")]
-    public Task<ActionResult> UpdateBook(int id, UpdateBook request)
+    public async Task<ActionResult> UpdateBook(int id, UpdateBook request)
     {
-        throw new NotImplementedException();
+        var book = await _bookRepository.GetByIdAsync(id);
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        var authors = new List<Application.Author>();
+        foreach (var authorId in request.Authors)
+        {
+            var author = await _authorRepository.GetByIdAsync(authorId);
+            if (author is null)
+            {
+                return BadRequest();
+            }
+
+            authors.Add(author);
+        }
+
+        var genres = new List<Application.Genre>();
+        foreach (var genreId in request.Genres)
+        {
+            var genre = await _genreRepository.GetByIdAsync(genreId);
+            if (genre is null)
+            {
+                return BadRequest();
+            }
+
+            genres.Add(genre);
+        }
+
+        book.Title = request.Title;
+        book.Authors = authors;
+        book.Genres = genres;
+        book.Description = request.Description;
+        book.Isbn = request.Isbn;
+        book.Price = request.Price;
+        book.Pages = request.Pages;
+        book.Year = request.Year;
+        await _bookRepository.UpdateAsync(book);
+        return NoContent();
     }
 
     [HttpDelete("{id:int}")]
-    public Task<ActionResult> DeleteBook(int id)
+    public async Task<ActionResult> DeleteBook(int id)
     {
-        throw new NotImplementedException();
+        var deleted = await _bookRepository.DeleteAsync(id);
+        if (!deleted)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
     }
 }
 
