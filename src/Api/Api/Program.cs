9e60531c1e0f70ecaaaa8a6fb62703e71c7b35b0
Volodymyr Domin <mindotale@gmail.com>
2023-10-29 14:20:45 +0200

    feat: Add the Radzen Lilibre.Web app

--- src/Api/Api/Program.cs
+++ src/Api/Api/Program.cs
@@ -1,6 +1,8 @@
 using Lilibre.Api.Configurations;
 using Lilibre.Persistence;
 
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -11,6 +13,14 @@ builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+    await Task.Delay(15000);
+    dbContext.Database.Migrate();
+}
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
@@ -19,12 +29,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var dbContext = services.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.EnsureCreated();
-}
 
 app.Run();
