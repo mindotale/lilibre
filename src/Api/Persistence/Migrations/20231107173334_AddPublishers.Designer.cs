1e8ce45f0044b1f2ec40da76809e19f92c1c61d1
Volodymyr Domin <mindotale@gmail.com>
2023-11-07 19:41:30 +0200

    feat: add a migration

--- src/Api/Persistence/Migrations/20231107173334_AddPublishers.Designer.cs
+++ src/Api/Persistence/Migrations/20231107173334_AddPublishers.Designer.cs
@@ -3,6 +3,7 @@ using Lilibre.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lilibre.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231107173334_AddPublishers")]
+    partial class AddPublishers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
