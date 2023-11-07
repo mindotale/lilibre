e47fe3ae2a0d36c135c5b5b80a10dc7e6b8be0d8
Volodymyr Domin <mindotale@gmail.com>
2023-11-07 19:54:59 +0200

    feat: add Publisher controller

---
+++ src/Api/Api/V1/Publishers/PublisherController.cs
@@ -0,0 +1,97 @@
+﻿using Lilibre.Application;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lilibre.Api.V1.Publishers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class PublisherController : ControllerBase
+{
+    private readonly IRepository<Application.Publisher, int> _publisherRepository;
+
+    public PublisherController(IRepository<Application.Publisher, int> publisherRepository)
+    {
+        _publisherRepository = publisherRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Publisher>>> GetPublishers(int offset = 0, int limit = 10)
+    {
+        var publishers = await _publisherRepository.GetAllAsync(offset, limit);
+        var response = publishers.Select(p => p.ToPublisher());
+        return Ok(response);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Publisher>> GetPublisher(int id)
+    {
+        var publisher = await _publisherRepository.GetByIdAsync(id);
+        if (publisher is null)
+        {
+            return NotFound();
+        }
+
+        var response = publisher.ToPublisher();
+        return Ok(response);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Publisher>> CreatePublisher(CreatePublisher request)
+    {
+        var publisher = new Application.Publisher
+        {
+            Id = 0,
+            Name = request.Name,
+            Description = request.Description,
+            Website = request.Website
+        };
+
+        var publisherId = await _publisherRepository.AddAsync(publisher);
+        var response = new Publisher(publisherId, publisher.Name, publisher.Description, publisher.Website);
+        return CreatedAtAction(nameof(GetPublisher), new { id = publisherId }, response);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdatePublisher(int id, UpdatePublisher request)
+    {
+        var publisher = await _publisherRepository.GetByIdAsync(id);
+        if (publisher is null)
+        {
+            return NotFound();
+        }
+
+        publisher.Name = request.Name;
+        publisher.Description = request.Description;
+        publisher.Website = request.Website;
+
+        await _publisherRepository.UpdateAsync(publisher);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeletePublisher(int id)
+    {
+        var deleted = await _publisherRepository.DeleteAsync(id);
+        if (!deleted)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+}
+
+public sealed record Publisher(int Id, string Name, string Description, string Website);
+
+public sealed record CreatePublisher(string Name, string Description, string Website);
+
+public sealed record UpdatePublisher(string Name, string Description, string Website);
+
+internal static class PublisherExtensions
+{
+    public static Publisher ToPublisher(this Application.Publisher publisher)
+    {
+        return new Publisher(publisher.Id, publisher.Name, publisher.Description, publisher.Website);
+    }
+}
