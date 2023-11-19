976ba8e66dd3e8d4f1fab1464ef8ff3a4ed3c6a3
Volodymyr Domin <mindotale@gmail.com>
2023-11-19 17:57:02 +0200

    feat: add a text/plain operation filter

---
+++ src/Api/Api/Swagger/OperationFilters/TextPlainOperationFilter .cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.OpenApi.Models;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Lilibre.Api.Swagger.OperationFilters;
+
+public class TextPlainOperationFilter : IOperationFilter
+{
+    private const string TextPlainMediaType = "text/plain";
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (operation.RequestBody is not null && operation.RequestBody.Content.TryGetValue(TextPlainMediaType, out var mediaType))
+        {
+            mediaType.Schema = new OpenApiSchema { Type = "string", Format = "text" };
+        }
+    }
+}
