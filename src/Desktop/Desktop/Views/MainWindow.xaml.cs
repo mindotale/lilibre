150d5eea0acc29eb3d2b42e4e5e99a2fa5772cb9
Volodymyr Domin <mindotale@gmail.com>
2023-12-04 22:59:38 +0200

    feat: add views

---
+++ src/Desktop/Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+using Desktop.ViewModels;
+
+namespace Desktop.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new AuthorViewModel();
+        }
+    }
+}
