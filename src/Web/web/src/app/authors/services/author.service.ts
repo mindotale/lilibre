b84c1995cbf8b02de8a843844380518677f93542
Volodymyr Domin <mindotale@gmail.com>
2023-12-10 18:35:52 +0200

    feat: add a service

---
+++ src/Web/web/src/app/authors/services/author.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Author } from '../models/author.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorService {
+  private apiUrl = 'your_api_url';
+
+  constructor(private http: HttpClient) { }
+
+  getAuthors(): Observable<Author[]> {
+    return this.http.get<Author[]>(`${this.apiUrl}/api/v1/authors`);
+  }
+
+  getAuthorById(id: number): Observable<Author> {
+    return this.http.get<Author>(`${this.apiUrl}/api/v1/authors/${id}`);
+  }
+
+  createAuthor(author: Author): Observable<Author> {
+    return this.http.post<Author>(`${this.apiUrl}/api/v1/authors`, author);
+  }
+
+  updateAuthor(id: number, author: Author): Observable<any> {
+    return this.http.put(`${this.apiUrl}/api/v1/authors/${id}`, author);
+  }
+
+  deleteAuthor(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/api/v1/authors/${id}`);
+  }
+}
