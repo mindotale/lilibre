ff69dc027b4890c18ab69d56c294e4846a0a57ba
Volodymyr Domin <mindotale@gmail.com>
2023-12-10 19:18:12 +0200

    feat: update routing modules

--- src/Web/web/src/app/app-routing.module.ts
+++ src/Web/web/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthorListComponent } from './authors/components/author-list/author-list.component';
+import { AuthorDetailsComponent } from './authors/components/author-details/author-details.component';
+import { AuthorCreateComponent } from './authors/components/author-create/author-create.component';
+import { AuthorUpdateComponent } from './authors/components/author-update/author-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/authors', pathMatch: 'full' },
+  { path: 'authors', component: AuthorListComponent },
+  { path: 'authors/:id', component: AuthorDetailsComponent },
+  { path: 'create', component: AuthorCreateComponent },
+  { path: 'update/:id', component: AuthorUpdateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
