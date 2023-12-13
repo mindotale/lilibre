d1e015a0bcc0a00e86ef7de3d9d1e906930672fc
Volodymyr Domin <mindotale@gmail.com>
2023-12-13 14:54:58 +0200

    refactor: improve the author service

--- src/Web/web/src/app/authors/services/author.service.ts
+++ src/Web/web/src/app/authors/services/author.service.ts
@@ -1,33 +1,63 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Author } from '../models/author.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
-  private apiUrl = 'http://localhost:5000';
+  private apiUrl = environment.apiUrl;
+  private apiVersion = 'v1';
+  private authorEndpoint = 'authors';
 
   constructor(private http: HttpClient) { }
 
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'Unknown error occurred';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+
   getAuthors(): Observable<Author[]> {
-    return this.http.get<Author[]>(`${this.apiUrl}/api/v1/authors`);
+    const url = `${this.apiUrl}/api/${this.apiVersion}/${this.authorEndpoint}`;
+    return this.http.get<Author[]>(url).pipe(
+      catchError(this.handleError)
+    );
   }
 
   getAuthorById(id: number): Observable<Author> {
-    return this.http.get<Author>(`${this.apiUrl}/api/v1/authors/${id}`);
+    const url = `${this.apiUrl}/api/${this.apiVersion}/${this.authorEndpoint}/${id}`;
+    return this.http.get<Author>(url).pipe(
+      catchError(this.handleError)
+    );
   }
 
   createAuthor(author: Author): Observable<Author> {
-    return this.http.post<Author>(`${this.apiUrl}/api/v1/authors`, author);
+    const url = `${this.apiUrl}/api/${this.apiVersion}/${this.authorEndpoint}`;
+    return this.http.post<Author>(url, author).pipe(
+      catchError(this.handleError)
+    );
   }
 
   updateAuthor(id: number, author: Author): Observable<any> {
-    return this.http.put(`${this.apiUrl}/api/v1/authors/${id}`, author);
+    const url = `${this.apiUrl}/api/${this.apiVersion}/${this.authorEndpoint}/${id}`;
+    return this.http.put(url, author).pipe(
+      catchError(this.handleError)
+    );
   }
 
   deleteAuthor(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/api/v1/authors/${id}`);
+    const url = `${this.apiUrl}/api/${this.apiVersion}/${this.authorEndpoint}/${id}`;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError)
+    );
   }
 }
